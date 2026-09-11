7788493093fb7f1b49da733a0e0392e9694ece15
Dev20024 <BobHendricks3000@gmail.com>
2026-09-10 20:27:55 -0400

    implemented alembic

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,3 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://bob@localhost:5432/MarketLensDatabase")
+DATABASE_URL = "postgresql+psycopg://marketlens:marketlens_dev@localhost:5432/marketlens"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
