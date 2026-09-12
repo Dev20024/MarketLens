3a7adba2496e7b752ce436425979d33893bffb5a
Dev20024 <BobHendricks3000@gmail.com>
2026-09-12 14:35:46 -0400

    Implemented Docker compose for containerizing fast api and postgres, also created .env

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://marketlens:marketlens_dev@localhost:5432/marketlens"
+#"postgresql+psycopg://marketlens:marketlens_dev@localhost:5432/marketlens"
+DATABASE_URL = os.environ("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
